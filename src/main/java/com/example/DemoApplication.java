bd1eff004dab17550204f5949b57392c7d92756e
maling <616779791@qq.com>
2017-04-11 21:55:18 +0800

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,7 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+public String index() {
+		return "hello world adfadf gggg";
+	}
+	
 	public String index() {
 		return "hello world";
 	}
